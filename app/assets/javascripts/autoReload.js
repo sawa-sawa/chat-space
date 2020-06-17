2618f731151b7b4e3794cd769de297aeada9ddef
sawa-sawa <orenannkadose.sawa14@gmail.com>
2020-06-17 18:21:18 +0900

    fix_autoReload

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -38,6 +38,7 @@ $(function(){
 
   let reloadMessages = function(){
     let last_message_id = $('.MessageBox:last').data("message-id");
+    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',
